eef409f197ca9ec579952c1a7d3a269681504de6
Richard Lois <richard.lois@ti.ukdw.ac.id>
2025-06-16 21:24:35 +0700

    feat: download model

---
+++ MLProject/download_model.py
@@ -0,0 +1,16 @@
+import mlflow
+
+EXPERIMENT_NAME = "Default"
+MODEL_NAME = "random_forest_model"
+TRACKING_URI = "https://dagshub.com/richardlois1/SML_Modelling.mlflow/"
+
+mlflow.set_tracking_uri(TRACKING_URI)
+client = mlflow.tracking.MlflowClient()
+
+experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
+runs = client.search_runs([experiment.experiment_id], order_by=["start_time DESC"], max_results=1)
+run_id = runs[0].info.run_id
+
+print("Latest run ID:", run_id)
+client.download_artifacts(run_id, MODEL_NAME, "downloaded_model")
+print("Model downloaded to: downloaded_model/")
